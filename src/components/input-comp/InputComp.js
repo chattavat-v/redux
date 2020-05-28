5a2fbf159e768fc49e81f36744171ae5c5cdc12c
chattavat-v <chattavat.v@gmail.com>
2020-05-28 15:26:43 +0700

    build button and input component

---
+++ src/components/input-comp/InputComp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const InputComp = (props) => {
+	return <input {...props} />;
+};
+
+InputComp.propTypes = {};
+
+export default InputComp;
