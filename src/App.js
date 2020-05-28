869adf1abc048db91af8787ced2b03ee309d1f3f
chattavat-v <chattavat.v@gmail.com>
2020-05-28 15:27:31 +0700

    import query data into app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchCats } from "./redux/actions/cat";
+import WrapperLayout from "./views/wrapper-layout/wrapperLayout";
+import FormView from "./views/form-view/FormView";
+import CardComp from "./components/card-comp/CardComp";
+import moment from "moment";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ({ fetchCats, cats }) => {
+	useEffect(() => {
+		fetchCats();
+	}, [fetchCats]);
 
-export default App;
+	return (
+		<WrapperLayout>
+			<h3 style={{textTransform: "uppercase"}}>Meow Story</h3>
+			<div className="wrapper-body">
+        <div className="wrapper-header">
+          <FormView />
+        </div>
+				<div className="wrapper-card">
+					{cats &&
+						cats.map((cat) => (
+							<CardComp key={cat.id}>
+								<div className="card_item">
+									<img src={cat.image} alt={cat.image} />
+									<p>{cat.name}</p>
+									<p>{moment(cat.create_date).format("YYYY-MM-DD")}</p>
+								</div>
+							</CardComp>
+						))}
+				</div>
+			</div>
+		</WrapperLayout>
+	);
+};
+
+App.propTypes = {
+	fetchCats: PropTypes.func.isRequired,
+	cats: PropTypes.array,
+};
+
+const mapStateToProps = (state) => ({
+	cats: state.cat.cats,
+});
+
+export default connect(mapStateToProps, { fetchCats })(App);
